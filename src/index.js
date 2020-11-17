969b8abfded7664712c6c67428d7e8b923bb5347
Artem <64410376+Artiss90@users.noreply.github.com>
2020-11-17 16:03:16 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// import { Theme } from './js/task';
+import './styles.css';
+import  '@ pnotify / core / dist / BrightTheme.css';
+import { alert, notice, info, success, error } from '@pnotify/core';
+// import BSN from 'bootstrap.native';
+
+document
+
+// Manually set the type.
+const myAlert = alert({
+  text: "I'm an alert.",
+  type: 'info'
+});
+
+// Automatically set the type.
+const myNotice = notice({
+  text: "I'm a notice."
+});
+
+const myInfo = info({
+  text: "I'm an info message."
+});
+
+const mySuccess = success({
+  text: "I'm a success message."
+});
+
+const myError = error({
+  text: "I'm an error message."
+});
+
+
